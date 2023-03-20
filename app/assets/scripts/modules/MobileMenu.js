835bcfdd196ba8db4137cd38f468216c2d54d44d
Marko <markonikovic@protonmail.com>
2023-03-20 11:43:29 +0100

    finished header section for mobile view

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,8 +1,19 @@
 class MobileMenu {
     constructor() {
-        document.querySelector(".site-header__menu-icon").addEventListener("click", function() {
-            console.log("The top right icon was clicked.")
-        })
+        this.menuIcon = document.querySelector(".site-header__menu-icon");
+        this.menuContent = document.querySelector(".site-header__menu-content");
+        this.siteHeader = document.querySelector(".site-header");
+        this.events();
+    }
+
+    events() {
+        this.menuIcon.addEventListener("click", () => this.toggleTheMenu()); // arrow function which doesn't manipulate "this" keyword
+    }
+
+    toggleTheMenu() {
+        this.menuContent.classList.toggle("site-header__menu-content--is-visible");
+        this.siteHeader.classList.toggle("site-header--is-expanded");
+        this.menuIcon.classList.toggle("site-header__menu-icon--close-x");
     }
 }
 
